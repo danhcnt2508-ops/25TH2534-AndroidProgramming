d2fbdef5d6ed1256486262d22c2724719c4d55a6
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-22 13:29:08 +0700

    update BT ListView DanhSachTinhThanh

--- DanhSachTinhThanh/app/src/main/java/edu/ntu/danhtm2534_dstinhthanh/MainActivity.java
+++ DanhSachTinhThanh/app/src/main/java/edu/ntu/danhtm2534_dstinhthanh/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.ntu.danhtm2534_dstinhthanh;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Hiển thị dữ liệu lên ListView
+        //B1: Cần có dữ liệu
+        // ??? từ cơ sở dữ liệu (SQL, noSQL, XML,...
+        //ở đây ta hard-code dữ liệu trực tiếp
+        //Cần biến phù hợp chứa dữ liệu
+        ArrayList<String> dsTenTinhThanhVN;     //khai báo
+        dsTenTinhThanhVN = new ArrayList<>();   //tạo thể hiện cụ thể
+            //thêm dữ liệu ở đây (đúng ra phải đọc dữ liệu từ 1 nguồn
+            dsTenTinhThanhVN.add("Hà Nội");
+            dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+            dsTenTinhThanhVN.add("Đồng Nai");
+            dsTenTinhThanhVN.add("Nghệ An");
+            dsTenTinhThanhVN.add("Bình Thuận");
+            dsTenTinhThanhVN.add("Ninh Thuận");
+            dsTenTinhThanhVN.add("Khánh Hòa");
+            dsTenTinhThanhVN.add("Sóc Trăng");
+            dsTenTinhThanhVN.add("Thành phố Cần Thơ");
+
+        //B2. tạo adapter
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this,
+                                                     android.R.layout.simple_list_item_1,
+                                                     dsTenTinhThanhVN
+                                                    );
+
+        //B3. Gắn vào điều khiển hiển thị ListView
+        //3.1 Tìm
+        ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
+        //3.2 gắn
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        //3.3 lắng nghe và xử lý sự kiện user tương tác
+
+
+
+
     }
 }
