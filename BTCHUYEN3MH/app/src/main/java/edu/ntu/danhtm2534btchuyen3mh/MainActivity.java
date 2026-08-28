7b34b4771ae979d7e0fc48a9114273c76e5b58a9
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-28 23:34:02 +0700

    Update MainActivity.java
    map nút bấm bằng các tìm điều khiển, gắng bộ lắng nghe sư kiện cho 2 nút ở màn hình chính

--- BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MainActivity.java
+++ BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.ntu.danhtm2534btchuyen3mh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +12,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien() {
+        //tìm điều khiển nút bấm
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //tìm điều khiển nút bấm
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //xử lý chuyển màn hình
+                //B1. Tạo một Intent có 2 tham số: MH hiện tại.this, MH chuyển tới.class
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2. Gửi
+                startActivity(intentMH2);
 
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //B1. Tạo một Intent
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2. Gửi
+                startActivity(intentMH3);
+
+            }
+        });
     }
 }
