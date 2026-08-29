c58b11ed52aebeab958b918b56a78a0217d889fc
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-29 20:23:34 +0700

    update BTCHUYEN3MH
    Done

--- BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MH2Activity.java
+++ BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MH2Activity.java
@@ -1,6 +1,9 @@
 package edu.ntu.danhtm2534btchuyen3mh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MH2Activity extends AppCompatActivity {
-
+    Button nutMH3_2;
+    void TimDieuKhien1() {
+        //tìm điều khiển nút bấm
+        nutMH3_2 = (Button) findViewById(R.id.btnMH3_2);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mh2);
+        TimDieuKhien1();
+        //Gắn bộ lắng nghe sự kiện
 
+        nutMH3_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //B1. Tạo một Intent
+                Intent intentMH3_2 = new Intent(MH2Activity.this, MH3Activity.class);
+                //B2. Gửi
+                startActivity(intentMH3_2);
+
+            }
+        });
     }
 }
--- BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MH3Activity.java
+++ BTCHUYEN3MH/app/src/main/java/edu/ntu/danhtm2534btchuyen3mh/MH3Activity.java
@@ -1,6 +1,9 @@
 package edu.ntu.danhtm2534btchuyen3mh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MH3Activity extends AppCompatActivity {
-
+    Button nutMH3_1;
+    void TimDieuKhien2() {
+        //tìm điều khiển nút bấm
+        nutMH3_1 = (Button) findViewById(R.id.btnMH3_1);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mh3);
+        TimDieuKhien2();
+        //Gắn bộ lắng nghe sự kiện
 
+        nutMH3_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMH3_1 = new Intent(MH3Activity.this, MainActivity.class);
+                startActivity(intentMH3_1);
+            }
+        });
     }
 }
