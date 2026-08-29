d34892414185abc84cd72ec456ff5f2ea656d731
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-29 22:53:49 +0700

    Update MainActivity, ActivityLogin

--- BT_Intent_Login/app/src/main/java/edu/ntu/danhtm2534bt_intent_login/ActivityLogin.java
+++ BT_Intent_Login/app/src/main/java/edu/ntu/danhtm2534bt_intent_login/ActivityLogin.java
@@ -1,6 +1,11 @@
 package edu.ntu.danhtm2534bt_intent_login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,37 @@ public class ActivityLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý đăng nhập
+                //Lấy dữ liệu
+                //B1. Tìm tham chiếu
+                EditText edtTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edtPass = (EditText) findViewById(R.id.edtPassword);
+                //lấy dữ liệu
+                String tenDangNhap = edtTenDN.getText().toString();
+                String mk = edtPass.getText().toString();
+
+                if(tenDangNhap.equals("danhtm") && mk.equals("123"))
+                {   //chuyển sang màn hình Home
+                    Intent iQuiz = new Intent(ActivityLogin.this, HomeActivity.class);
+                    //gói dữ liệu vào iQuiz, dạng key - value; key được dùng để bên kia lọc ra dữ liệu
+                    iQuiz.putExtra("ten_dang_nhap", tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap", mk);
+                    //gửi đi
+                    startActivity(iQuiz);
+                }
+                else
+                {
+                    Toast.makeText(ActivityLogin.this, "Mật khẩu không đúng. Vui lòng nhập lại.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
--- BT_Intent_Login/app/src/main/java/edu/ntu/danhtm2534bt_intent_login/MainActivity.java
+++ BT_Intent_Login/app/src/main/java/edu/ntu/danhtm2534bt_intent_login/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.ntu.danhtm2534bt_intent_login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //tìm nút Đăng nhập
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //sự kiện
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLogin = new Intent(MainActivity.this, ActivityLogin.class);
+                startActivity(iLogin);
+            }
+        });
 
     }
 }
